2580f65e28b071b25b7001ed95767219eef6ca13
Larger One <inhortte@gmail.com>
2010-12-23 20:26:43 -0600

    more progress on the dropdown menu and directory heierarchy

--- helpers/menu.rb
+++ helpers/menu.rb
@@ -2,29 +2,32 @@ module Sinatra
   module MenuHelper
     def get_menus(dir, order = nil)
       Dir.entries(dir).map do |f|
-        unless f =~ /^\./
-          if File.directory? File.join(dir, f)
-            { f => get_menus(File.join(dir, f)) }
+        m = /^(.+)\.haml/.match(f)
+        if m
+          menu = m[1]
+          if File.directory? File.join(dir, menu)
+            { menu => get_menus(File.join(dir, menu)) }
           else
-            m = /^(.+)\.haml/.match(f)
-            if m
-              m[1]
-            end
+            menu
           end
         end
-      end.select { |e| e }
+      end.select { |m| m }
     end
 
     def sort_menus(menus)
       menus.sort do |a, b|
-        a = a.keys[0] if a.class == Hash
-        b = b.keys[0] if b.class == Hash
+        a = a.keys[0] if a.is_a? Hash
+        b = b.keys[0] if b.is_a? Hash
         a <=> b
       end
     end
 
     def format_menu_name(m)
       m.gsub(/_/, " ").split(/ /).map { |w| w.capitalize }.join(' ')
+    end
+
+    def get_menu_name(m)
+      (m.is_a? Hash) ? m.keys[0] : m
     end
   end
 
